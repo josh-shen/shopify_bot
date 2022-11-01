13d147e2afa080d3c93432ca926c3367cd42546d
Joshua Shen <59071877+josh-shen@users.noreply.github.com>
2022-11-01 12:17:25 -0700

    Update for protected routes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,18 +1,20 @@
 import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./Home"
+import ProtectedRoute from "../auth_utils/Protection"
 
-function App() {
+export default function App() {
 
   return (
       <div>
         <Router>
             <Routes>
-              <Route exact path="/" element={<Home />} />
+              <Route element={<ProtectedRoute />}>
+                <Route path="/bot" element={<Home />} /> 
+              </Route>
+              <Route exact path="/" element={<Clock />} />
             </Routes>
         </Router>
       </div>
   )
 }
-
-export default App
